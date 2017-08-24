ed39772c71deae7490ee82313c8cf1800395dc5a
xulangping <xlp@mail.ustc.edu.cn>
2017-08-24 14:46:44 +0800

    两两配对
    初级版本

--- diff.py
+++ diff.py
@@ -20,14 +20,58 @@ def delete_ht(s1, s2):
     a1_re = a1_re[i:]
     a2_re = a2_re[i:]
     return a1_re[::-1], a2_re[::-1]
-f = open('aa_nlc_nlc2', encoding = 'UTF-8')
+
+def flatten(l):
+    if isinstance(l, list):
+        for el in l:
+            if isinstance(el, list):
+                for sub in flatten(el):
+                    yield sub
+            else:
+                yield el
+    else:
+        yield l
+
+
+def group_2(s):
+    a = []
+    if len(s) % 2 ==0:
+        for i in range(0, len(s), 2):
+            a.append([s[i], s[i + 1]])
+    else:
+        for i in range(0, len(s) - 1, 2):
+            a.append([s[i], s[i + 1]])
+        a.append(s[-1])
+    return a
+def delete_mid(s1, s2):
+    a1 = group_2(s1)
+    a2 = group_2(s2)
+    a1_left = [i for i in a1 if i not in a2]
+    a2_left = [i for i in a2 if i not in a1]
+    return a1_left, a2_left
+
+
+f = open('aa_nlc_nlc2', encoding='UTF-8')
 lines = f.readlines()
+num_0 = 0
+num_1 = 0
+num_2 = 0
 for line in lines:
     [sentence1, sentence2] = line.split('\t')
     sentence2 = sentence2.split('\n')[0]
     if sentence1 == sentence2:
-        pass
+        num_0 += 1
     else:
         sentence1, sentence2 = delete_ht(sentence1, sentence2)
-        print(sentence1)
-        print(sentence2)
+        if len(sentence1) > 1 and len(sentence2) > 1:
+            if set(sentence1) == set(sentence2):
+                num_1 += 1
+            else:
+                sentence1, sentence2 = delete_mid(sentence1, sentence2)
+                print(sentence1)
+                print(sentence2)
+                print(line)
+                num_2 += 1
+        else:
+            num_1 += 1
+print(num_0, num_1, num_2)
